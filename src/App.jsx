c0ae43f48e3bbba74cef239bc81e9afb6e6f1b20
Rafael Estrada <restradap1600@gmail.com>
2021-03-07 21:07:19 -0600

    change App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
-import NovelaHome from './components/Novela/NovelaHome';
 import './App.scss';
+import Novela from './components/Novela/Novela';
+import Novelas from './components/Novelas/Novelas';
 
 function App() {
     return (
         <Router>
             <Header />
             <Switch>
-                <Route path="/">
-                    <NovelaHome title="We Can Do It Kokichi!" />
+                <Route path="/novelas/:id">
+                    <Novela />
+                </Route>
+                <Route path="/novelas">
+                    <Novelas />
                 </Route>
             </Switch>
         </Router>

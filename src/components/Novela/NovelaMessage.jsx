f19111f37f58e0b3e5b99520304678f0576caef1
Rafael Estrada <ntaticat@gmail.com>
2021-04-26 22:48:13 -0500

    add NovelaMessage component

---
+++ src/components/Novela/NovelaMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NovelaMessage.scss';
+
+const NovelaMessage = () => {
+    return (
+        <div className="stage-message">
+            <span>Aquí se mostrarán los mensajes...</span>
+        </div>
+    );
+};
+
+export default NovelaMessage;

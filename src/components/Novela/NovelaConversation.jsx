64eee0af30dd9120825edba79516ef0e737b6c76
Rafael Estrada <ntaticat@gmail.com>
2021-04-26 22:50:24 -0500

    add NovelaConversation component

---
+++ src/components/Novela/NovelaConversation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NovelaConversation.scss';
+
+const NovelaConversation = () => {
+    return (
+        <div className="novel-conversation">
+            <p className="novel-conversation-line">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, commodi quae! Alias est ducimus veritatis voluptas soluta! Debitis, in. Aspernatur odio eligendi voluptates et error illum perspiciatis porro quidem ullam.
+                </p>
+        </div>
+    );
+};
+
+export default NovelaConversation;

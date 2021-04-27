7ede7cb213d6d0c38fe9ec107c60857974f148f1
Rafael Estrada <ntaticat@gmail.com>
2021-04-26 22:55:12 -0500

    update NovelaStage component

--- src/components/Novela/NovelaStage.jsx
+++ src/components/Novela/NovelaStage.jsx
@@ -1,48 +1,56 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import './NovelaStage.scss';
+import NovelaMessage from './NovelaMessage';
+import NovelaDecisions from './NovelaDecisions';
+import NovelaOptions from './NovelaOptions';
+import NovelaConversation from './NovelaConversation';
+// import axiosClient from '../../config/axios';
 
-const NovelaStage = () => {
+const NovelaStage = ({ novelaId }) => {
+
+    // const [escena, setEscena] = useState({});
+    // const [recurso, setRecurso] = useState({});
+
+    // useEffect(() => {
+    //     consultarInformacionNovela(novelaId);
+    // }, [novelaId]);
+
+    // const consultarInformacionNovela = async (id) => {
+    //     try {
+    //         const info = {
+    //             recursoInfo: {
+    //                 escena: "6047c0c82a84262d28f86472",
+    //                 tipoRecursoActual: "conversacion",
+    //             },
+    //             recursoData: {
+    //                 texto: "necesario en la peticion",
+    //                 recursoPosterior: null,
+    //                 tipoRecursoPosterior: null
+    //             }
+    //         };
+
+    //         const resultado = await axiosClient.post(`/api/recursos`, info);
+    //         const data = await resultado.data;
+    //         console.log("recursos", data);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
 
     return (
-        <div className="stage">
-            <div className="stage-message">
-                <span>Aquí se mostrarán los mensajes...</span>
+        <div className="novel-container">
+            <div className="stage">
+                <NovelaMessage />
+                <div className="stage-character">
+                    <img src="https://i.pinimg.com/originals/55/6b/bd/556bbd57bb1d10813cb74744a573db33.png" alt="Kokichi Sprite" />
+                </div>
+                <div className="stage-character-name">
+                    <span>Kokichi Oma</span>
+                </div>
+                <NovelaDecisions />
             </div>
-
-            <div className="stage-character">
-                <img src="https://i.pinimg.com/originals/55/6b/bd/556bbd57bb1d10813cb74744a573db33.png" alt="Kokichi Sprite" />
-            </div>
-
-            <div className="stage-character-name">
-                <span>Kokichi Oma</span>
-            </div>
-
-            <div className="novel-decisions">
-                <label htmlFor="decision1" className="novel-decision-radio" id="labeldecision1">
-                    <input type="radio" id="decision1" name="decision" value="1" />
-                    <span className="novel-decision-info">
-                        Esta es una decisión
-                            </span>
-                </label>
-                <label htmlFor="decision2" className="novel-decision-radio" id="labeldecision2">
-                    <input type="radio" id="decision2" name="decision" value="2" />
-                    <span className="novel-decision-info">
-                        Esta es una decisión
-                            </span>
-                </label>
-                <label htmlFor="decision3" className="novel-decision-radio" id="labeldecision3">
-                    <input type="radio" id="decision3" name="decision" value="3" />
-                    <span className="novel-decision-info">
-                        Esta es una decisión
-                            </span>
-                </label>
-                <label htmlFor="decision4" className="novel-decision-radio" id="labeldecision4">
-                    <input type="radio" id="decision4" name="decision" value="4" />
-                    <span className="novel-decision-info">
-                        Esta es una decisión
-                            </span>
-                </label>
-            </div>
+            <NovelaConversation />
+            <NovelaOptions />
         </div>
     );
 };
